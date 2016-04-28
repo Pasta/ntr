71d5fc5e3c86a97bedf600afb1f2b635bccd713a
Vincent Coste <coste.vincent@gmail.com>
2016-04-27 22:27:17 -0700

    WIP employee form

--- client/app/bundles/Dashboard/actions/__tests__/employeesActionCreators.spec.js
+++ client/app/bundles/Dashboard/actions/__tests__/employeesActionCreators.spec.js
@@ -1,14 +1,31 @@
 import { expect } from 'chai';
-import { employeeSubmitting } from '../employeesActionCreators';
+import { employeeSubmitting, employeeSubmitSuccess, employeeSubmitFailure } from '../employeesActionCreators';
 import deepFreeze from 'deep-freeze';
 
 describe('Employee', () => {
   describe('Employee Actions', () => {
     it('sets the employee in submitting', () => {
-      expect(employeeSubmitting(employee)).to.eql(
+      expect(employeeSubmitting()).to.eql(
         {
-          type: 'tsundoku/EMPLOYEE_SUBMITTING',
+          type: 'tsundoku/EMPLOYEE_SUBMITTING'
+        }
+      );
+    });
+    it('sets the employee to submited', () => {
+      let employee = { first_name: "monkey", last_name:"money"};
+      expect(employeeSubmitSuccess(employee)).to.eql(
+        {
+          type: 'tsundoku/EMPLOYEE_SUBMIT_SUCCESS',
           employee: employee
+        }
+      );
+    });
+    it('sets the employee failed to submit', () => {
+      let error = { error: "monkey money"};
+      expect(employeeSubmitFailure(error)).to.eql(
+        {
+          type: 'tsundoku/EMPLOYEE_SUBMITTION_FAILED',
+          error: error
         }
       );
     });
--- client/app/bundles/Dashboard/actions/employeesActionCreators.js
+++ client/app/bundles/Dashboard/actions/employeesActionCreators.js
@@ -1,15 +1,36 @@
-// const EMPLOYEE_SUBMITTED = 'tsundoku/EMPLOYEE_SUBMITTED';
-// export function employeeSubmitted(employee) {
-//   return {
-//     type: EMPLOYEE_SUBMITTED,
-//     employee: employee
-//   };
-// }
+import employeesRequestManager from 'lib/requestManagers/employeesRequestManager'
 
 const EMPLOYEE_SUBMITTING = 'tsundoku/EMPLOYEE_SUBMITTING';
-export function employeeSubmitting(employee) {
+export function employeeSubmitting() {
   return {
-    type: EMPLOYEE_SUBMITTING,
+    type: EMPLOYEE_SUBMITTING
+  };
+}
+
+const EMPLOYEE_SUBMIT_SUCCESS = 'tsundoku/EMPLOYEE_SUBMIT_SUCCESS';
+export function employeeSubmitSuccess(employee) {
+  return {
+    type: EMPLOYEE_SUBMIT_SUCCESS,
     employee: employee
+  };
+}
+
+const EMPLOYEE_SUBMITTION_FAILED = 'tsundoku/EMPLOYEE_SUBMITTION_FAILED';
+export function employeeSubmitFailure(error) {
+  return {
+    type: EMPLOYEE_SUBMITTION_FAILED,
+    error: error
+  };
+}
+
+export function submitEmployee(employee) {
+  return dispatch => {
+    dispatch(employeeSubmitting());
+    return (
+      employeesRequestManager
+        .submitEntity({ employee })
+        .then(res => dispatch(employeeSubmitSuccess(res.data)))
+        .catch(res => dispatch(employeeSubmitFailure(res.data)))
+    );
   };
 }
